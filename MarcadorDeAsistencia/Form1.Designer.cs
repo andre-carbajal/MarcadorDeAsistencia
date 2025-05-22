ef02c22372d4912e17138fc4a13f4b8f6889f085
andre-carbajal <carbajalvargasandre@gmail.com>
2025-05-22 07:02:01 -0500

    Añadir soporte x64 y mejorar la interfaz de usuario
    Se han agregado configuraciones de plataforma x64 en el archivo de solución `MarcadorDeAsistencia.sln` para las configuraciones de depuración y liberación.
    
    En `Form1.Designer.cs`, se han incorporado nuevos controles de interfaz de usuario, como `PictureBox`, `GroupBox`, `Label` y varios botones, mejorando la funcionalidad del control de asistencia.
    
    Se ha creado un nuevo archivo de recursos `Form1.resx` para gestionar recursos en formato XML, y se han actualizado las propiedades del proyecto en `MarcadorDeAsistencia.csproj` para incluir configuraciones específicas para x64 y el nuevo archivo de recursos como recurso embebido.

--- MarcadorDeAsistencia/Form1.Designer.cs
+++ MarcadorDeAsistencia/Form1.Designer.cs
@@ -28,13 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.btnInicioDescanso = new System.Windows.Forms.Button();
+            this.btnFinDescanso = new System.Windows.Forms.Button();
+            this.btnActivarCamara = new System.Windows.Forms.Button();
+            this.btnDesactivarCamara = new System.Windows.Forms.Button();
+            this.btnRegistrarAsistencia = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 397);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(415, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 74);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sistema: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnFinDescanso);
+            this.groupBox2.Controls.Add(this.btnInicioDescanso);
+            this.groupBox2.Controls.Add(this.btnSalida);
+            this.groupBox2.Controls.Add(this.btnEntrada);
+            this.groupBox2.Location = new System.Drawing.Point(415, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(373, 77);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tipo de Asistencia: ";
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.Location = new System.Drawing.Point(6, 19);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(165, 23);
+            this.btnEntrada.TabIndex = 0;
+            this.btnEntrada.Text = "Entrada";
+            this.btnEntrada.UseVisualStyleBackColor = true;
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.Location = new System.Drawing.Point(202, 19);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(165, 23);
+            this.btnSalida.TabIndex = 1;
+            this.btnSalida.Text = "Salida";
+            this.btnSalida.UseVisualStyleBackColor = true;
+            // 
+            // btnInicioDescanso
+            // 
+            this.btnInicioDescanso.Location = new System.Drawing.Point(6, 48);
+            this.btnInicioDescanso.Name = "btnInicioDescanso";
+            this.btnInicioDescanso.Size = new System.Drawing.Size(165, 23);
+            this.btnInicioDescanso.TabIndex = 2;
+            this.btnInicioDescanso.Text = "Inicio de Descanso";
+            this.btnInicioDescanso.UseVisualStyleBackColor = true;
+            // 
+            // btnFinDescanso
+            // 
+            this.btnFinDescanso.Location = new System.Drawing.Point(202, 48);
+            this.btnFinDescanso.Name = "btnFinDescanso";
+            this.btnFinDescanso.Size = new System.Drawing.Size(165, 23);
+            this.btnFinDescanso.TabIndex = 3;
+            this.btnFinDescanso.Text = "Fin de Descanso";
+            this.btnFinDescanso.UseVisualStyleBackColor = true;
+            // 
+            // btnActivarCamara
+            // 
+            this.btnActivarCamara.Location = new System.Drawing.Point(12, 415);
+            this.btnActivarCamara.Name = "btnActivarCamara";
+            this.btnActivarCamara.Size = new System.Drawing.Size(160, 23);
+            this.btnActivarCamara.TabIndex = 4;
+            this.btnActivarCamara.Text = "Activar Cámara";
+            this.btnActivarCamara.UseVisualStyleBackColor = true;
+            // 
+            // btnDesactivarCamara
+            // 
+            this.btnDesactivarCamara.Location = new System.Drawing.Point(249, 415);
+            this.btnDesactivarCamara.Name = "btnDesactivarCamara";
+            this.btnDesactivarCamara.Size = new System.Drawing.Size(160, 23);
+            this.btnDesactivarCamara.TabIndex = 5;
+            this.btnDesactivarCamara.Text = "Desactivar Cámara";
+            this.btnDesactivarCamara.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrarAsistencia
+            // 
+            this.btnRegistrarAsistencia.Location = new System.Drawing.Point(508, 190);
+            this.btnRegistrarAsistencia.Name = "btnRegistrarAsistencia";
+            this.btnRegistrarAsistencia.Size = new System.Drawing.Size(182, 23);
+            this.btnRegistrarAsistencia.TabIndex = 6;
+            this.btnRegistrarAsistencia.Text = "Registrar Asistencia";
+            this.btnRegistrarAsistencia.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnRegistrarAsistencia);
+            this.Controls.Add(this.btnDesactivarCamara);
+            this.Controls.Add(this.btnActivarCamara);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control de Asistencia";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnFinDescanso;
+        private System.Windows.Forms.Button btnInicioDescanso;
+        private System.Windows.Forms.Button btnSalida;
+        private System.Windows.Forms.Button btnEntrada;
+        private System.Windows.Forms.Button btnActivarCamara;
+        private System.Windows.Forms.Button btnDesactivarCamara;
+        private System.Windows.Forms.Button btnRegistrarAsistencia;
     }
 }
 
